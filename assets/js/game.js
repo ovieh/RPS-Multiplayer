1729e96c2f4c95e1fe6600ed4db910bc8c1c5c9f
Ovieh <ovieh.mosley@gmail.com>
2017-09-24 22:01:10 -0400

    initial site files

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+// variables: choice, losses, wins,
+// objects: player :name, game
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDSERWkkCXaXjNL07E9CmfYbygVaXgjBYA",
+    authDomain: "rps-rcb-2611b.firebaseapp.com",
+    databaseURL: "https://rps-rcb-2611b.firebaseio.com",
+    projectId: "rps-rcb-2611b",
+    storageBucket: "rps-rcb-2611b.appspot.com",
+    messagingSenderId: "951207921709"
+  };
+  firebase.initializeApp(config);
